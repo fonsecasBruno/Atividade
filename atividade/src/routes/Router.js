50d2e500af929375929678defaed318b19dd7093
fonsecasBruno <brunofsouza669@gmail.com>
2023-09-26 20:13:24 -0300

    First Commit

---
+++ atividade/src/routes/Router.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
+
+export default function Router() {
+  return (
+    
+    <NavigationContainer>
+
+
+
+    </NavigationContainer>
+
+  )
+}
+
